820dcf7d70e4c67288e283d21c4b741fd3d401e8
JM Smaron <jmsmaron2003@gmail.com>
2025-05-09 14:45:10 +0600

    more data fetched from api

--- better_des.js
+++ better_des.js
@@ -8,23 +8,39 @@ function start() {
         .catch(err => alert("Country not found!"));
 }
 
+
+
 function process(data) {
     const display = document.getElementById("displayArea");
     display.textContent = "";
 
     data.forEach(country => {
+        const countryName = country.name.common;
+        const capital = country.capital ? country.capital[0] : "N/A";
+        const population = country.population.toLocaleString();
+        const region = country.region;
+        const flag = country.flags.png;
+
+        const currencyCode = Object.keys(country.currencies)[0];
+        const currency = country.currencies[currencyCode];
+        const currencyDisplay = `${currency.name} (${currency.symbol})`;
+        const languageList = Object.values(country.languages || {}).join(", ");
+        const area = `${country.area.toLocaleString()} km²`;
+        const demonym = country.demonyms?.eng?.m || "N/A";
+
         const div = document.createElement("div");
         div.classList.add("countryCard");
 
-        const countryName = country.name.common;
-        const capital = country.capital ? country.capital[0] : "N/A";
-
         div.innerHTML = `
             <h2>${countryName}</h2>
-            <p><strong>Region:</strong> ${country.region}</p>
+            <img src="${flag}" alt="Flag of ${countryName}">
             <p><strong>Capital:</strong> ${capital}</p>
-            <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-            <img src="${country.flags.png}" alt="Flag of ${countryName}">
+            <p><strong>Region:</strong> ${region}</p>
+            <p><strong>Population:</strong> ${population}</p>
+            <p><strong>Currency:</strong> ${currencyDisplay}</p>
+            <p><strong>Languages:</strong> ${languageList}</p>
+            <p><strong>Area:</strong> ${area}</p>
+            <p><strong>Demonym:</strong> ${demonym}</p>
         `;
 
         display.appendChild(div);
